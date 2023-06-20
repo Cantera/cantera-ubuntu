f3551c3cdb03c3bcbce2a5088d7a440e79fa5dd1
Ray Speth <speth@mit.edu>
2023-06-20 16:47:03 -0400

    Drop image for EOL 21.10 release; add 23.04 image

--- build_images.sh
+++ build_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build --build-arg ubuntu_release=20.04 -t $USER/ctppa:ubuntu20.04 .
-docker build --build-arg ubuntu_release=21.10 -t $USER/ctppa:ubuntu21.10 .
 docker build --build-arg ubuntu_release=22.04 -t $USER/ctppa:ubuntu22.04 .
 docker build --build-arg ubuntu_release=22.10 -t $USER/ctppa:ubuntu22.10 .
+docker build --build-arg ubuntu_release=23.04 -t $USER/ctppa:ubuntu23.04 .
