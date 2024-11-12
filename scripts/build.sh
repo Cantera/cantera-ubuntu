350d8cdde6f5942b3d5f660477f458130e2814b5
Ray Speth <speth@mit.edu>
2024-11-11 21:28:36 -0500

    Set CT_GIT_COMMIT for use by SCons

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,4 +7,6 @@ die () {
 
 /src/prepare.sh || die "prepare failed"
 cd /src/packaging/cantera
-debuild -uc -us
+
+CT_GIT_COMMIT=`git --git-dir=/src/cantera/.git rev-list -n1 $BASE_REF`
+debuild --set-envvar=CT_GIT_COMMIT=$CT_GIT_COMMIT -uc -us
