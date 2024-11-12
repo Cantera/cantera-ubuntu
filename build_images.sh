9eb37f75742fd0f0d196e86cc5c10e41e001184d
Ray Speth <speth@mit.edu>
2024-11-11 21:26:00 -0500

    Add image templates for recent Ubuntu releases

--- build_images.sh
+++ build_images.sh
@@ -2,5 +2,5 @@
 
 docker build --build-arg ubuntu_release=20.04 -t $USER/ctppa:ubuntu20.04 .
 docker build --build-arg ubuntu_release=22.04 -t $USER/ctppa:ubuntu22.04 .
-docker build --build-arg ubuntu_release=22.10 -t $USER/ctppa:ubuntu22.10 .
-docker build --build-arg ubuntu_release=23.04 -t $USER/ctppa:ubuntu23.04 .
+docker build --build-arg ubuntu_release=24.04 -t $USER/ctppa:ubuntu24.04 .
+docker build --build-arg ubuntu_release=24.10 -t $USER/ctppa:ubuntu24.10 .
