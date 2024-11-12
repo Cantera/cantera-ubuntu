a2237a3434fdbc5622df6eae6d6792c3c02b51d2
Ray Speth <speth@mit.edu>
2024-11-11 21:26:53 -0500

    Handle example_data repository

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -8,7 +8,12 @@ die () {
 mkdir -p /src/packaging
 cd /src/cantera
 git archive $PACKAGING_BRANCH --output=../packaging/tmp.tar --prefix=cantera/ || die "Branch ${PACKAGING_BRANCH} not found"
+
+cd data/example_data
+git archive $BASE_REF --output=../../../packaging/tmp-data.tar --prefix=cantera/data/example_data/ || die "Tag ${BASE_REF} not found in example_data"
+
 cd /src/packaging
 rm -rf cantera
 cp /src/cantera-ubuntu/cantera_${FULL_VERSION}.orig.tar.gz . || die "Pristine tarball not found"
 tar xf tmp.tar
+tar xf tmp-data.tar
