f3243e72240bec16a09f89a36552e4a489912b71
Ray Speth <speth@mit.edu>
2024-11-11 21:28:03 -0500

    Bump default build version to 3.1.0b1

--- vars.sh
+++ vars.sh
@@ -2,13 +2,13 @@
 
 # This is the version used in the name of the branch containing the packaging data,
 # e.g. for the branch 'ubuntu22.10-ct3.0', this is '3.0'.
-export SHORT_VERSION=3.0
+export SHORT_VERSION=3.1
 
 # Ubuntu-compatible version name (insert '~' before a/b tags, e.g. '3.0.0~a5')
-export FULL_VERSION=3.0.0~b1
+export FULL_VERSION=3.1.0~b1
 
 # For releases uploaded to Launchpad, this should be an "upstream" tag name, e.g. v3.0.0b1
-export BASE_REF=v3.0.0b1
+export BASE_REF=v3.1.0b1
 
 PPA_TARGET=cantera-team/cantera-unstable
 #PPA_TARGET=cantera-team/cantera
